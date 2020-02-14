ef7e50d9be63733ffba2491b5453a8aa269193e4
HarvesteR <felipe@floatingoriginstudios.com>
2020-02-13 20:28:48 -0600

    More tweaks to test plugin project.

--- examples/BalsaPluginTest/MenuMessageTest.cs
+++ examples/BalsaPluginTest/MenuMessageTest.cs
@@ -13,27 +13,29 @@ namespace BalsaPluginTest
 		public override void OnShow(Action proceed)
 		{
 			PopupDialog.Create("Addon Test",
-				"This message is brought to you by BalsaAddonTest.dll, for all your addon testing needs!" 
+				"This message is brought to you by BalsaAddonTest.dll, for all your addon testing needs!"
 				+ "\n\n"
 				+ ("Check out this plugin's source code in the Balsa.SDK, available on Github.".RTColor(BalsaColors.TrainingBlue).RTSizePct(80f))
-				
+
 				, IconSprite.Info, BalsaColors.TrainingBlue,
 					new PopupDialogOption("Cool", proceed),
-					new PopupDialogOption("Open Github", openGitHub + proceed)); // don't forget to call proceed on these messages, otherwise the game can get stuck!
+					new PopupDialogOption("Open Github", () =>
+					{
+						openGitHub();
+						proceed?.Invoke();  // don't forget to call proceed on these messages, otherwise the game can get stuck!
+					}));
 		}
 
 		private void openGitHub()
 		{
 			// calling explorer with an url opens it in the default browser. Use this new power responsibly!
-			System.Diagnostics.Process.Start("explorer.exe", "https://floatingorigininteractive.github.io/Balsa.SDK");
+			//System.Diagnostics.Process.Start("explorer.exe", "https://floatingorigininteractive.github.io/Balsa.SDK");
 
-			// Alternatively, you can use Steam's internal browser.
-			// you'll need to add a reference to Facepunch.Steamworks to this project
-			//if (BalsaAddons.Steam.SteamworksSetup.Init)
-			//{
-			//	Steamworks.SteamFriends.OpenWebOverlay("https://steamcommunity.com/workshop/browse/?appid=977920&browsesort=trend&section=readytouseitems&requiredtags%5B%5D=Missions");
-
-			//}
+			// Alternatively, you can use Steam's internal browser in the overlay.
+			if (BalsaAddons.Steam.SteamworksSetup.Init)
+			{
+				Steamworks.SteamFriends.OpenWebOverlay("https://floatingorigininteractive.github.io/Balsa.SDK");
+			}
 		}
 	}
 }
